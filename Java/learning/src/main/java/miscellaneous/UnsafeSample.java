b964a1b240f7ca2ca0b710f96df036139793fd77
michael.ren <michael_ww@163.com>
2025-07-08 09:47:08 +0800

    java and c# learning

---
+++ Java/learning/src/main/java/miscellaneous/UnsafeSample.java
@@ -0,0 +1,8 @@
+package miscellaneous;
+
+public class UnsafeSample {
+
+    public static void main(String[] args) {
+        System.out.println("Unsafe API is not used. Please use standard Java APIs.");
+    }
+}
